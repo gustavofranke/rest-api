354feef812bc90d69d46eba601550992558e4939
Gustavo Franke <franke.gustavo@gmail.com>
2020-10-23 21:53:47 +0100

    serve sample json

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,47 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.Monad.Trans
+import Data.Aeson hiding (json)
+import Data.IORef
+import Data.Monoid ((<>))
+import Data.Text (Text, pack)
+import qualified Data.Text as T
+import GHC.Generics
 import Web.Spock
 import Web.Spock.Config
 
-import Control.Monad.Trans
-import Data.IORef
-import Data.Monoid
-import qualified Data.Text as T
+data Person = Person
+  { name :: Text,
+    age :: Int
+  }
+  deriving (Generic, Show)
+
+instance ToJSON Person
+
+instance FromJSON Person
+
+
+-- *Main> :set -XOverloadedStrings
+-- *Main> encode Person { name = "Leela", age = 25 }
+-- "{\"age\":25,\"name\":\"Leela\"}"
+-- *Main> decode "{ \"name\": \"Amy\", \"age\": 30 }" :: Maybe Person
+-- Just (Person {name = "Amy", age = 30})
+-- *Main> 
+-- *Main> decode "{\"age\":25,\"name\":\"Leela\"}" :: Maybe Person
+-- Just (Person {name = "Leela", age = 25})
+-- *Main> 
+-- *Main> decode "{ \"name\": \"Amy\", \"age\": 30 }" :: Maybe Value
+-- Just (Object (fromList [("age",Number 30.0),("name",String "Amy")]))
+-- *Main> 
 
 data MySession = EmptySession
 data MyAppState = DummyAppState (IORef Int)
+
+type Api = SpockM () () () ()
+type ApiAction a = SpockAction () () () a
 
 main :: IO ()
 main = do
@@ -26,3 +57,5 @@ app =
        do (DummyAppState ref) <- getState
           visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
           text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+     get "people" $
+       do json $ Person { name = "Fry", age = 25 }
