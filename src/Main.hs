a97f1ef517d47426e0bab9a3c30d8ba7d2858365
Gustavo Franke <franke.gustavo@gmail.com>
2020-10-23 22:07:13 +0100

    add post

--- src/Main.hs
+++ src/Main.hs
@@ -40,8 +40,8 @@ instance FromJSON Person
 data MySession = EmptySession
 data MyAppState = DummyAppState (IORef Int)
 
-type Api = SpockM () () () ()
-type ApiAction a = SpockAction () () () a
+type Api = SpockM () MySession MyAppState ()
+type ApiAction a = SpockAction () MySession MyAppState a
 
 main :: IO ()
 main = do
@@ -49,13 +49,19 @@ main = do
   spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
   runSpock 8080 (spock spockCfg app)
 
-app :: SpockM () MySession MyAppState ()
+app :: Api
 app =
-  do get root $
-         text "Hello World!"
-     get ("hello" <//> var) $ \name ->
-       do (DummyAppState ref) <- getState
-          visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
-          text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
-     get "people" $
-       do json $ Person { name = "Fry", age = 25 }
+  do
+    get root $
+      text "Hello World!"
+    get ("hello" <//> var) $ \name ->
+      do
+        (DummyAppState ref) <- getState
+        visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i + 1, i + 1)
+        text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+    get "people" $
+      do json [Person {name = "Fry", age = 25}, Person {name = "Bender", age = 4}]
+    post "people" $
+      do
+        thePerson <- jsonBody' :: ApiAction Person
+        text $ "Parsed: " <> pack (show thePerson)
