1170265b4b9086ec0a8d79cfb0a306c5d09fb2e1
Gustavo Franke <franke.gustavo@gmail.com>
2020-10-25 20:33:39 +0000

    format code

--- src/Main.hs
+++ src/Main.hs
@@ -1,35 +1,38 @@
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE EmptyDataDecls             #-}
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE QuasiQuotes                #-}
-{-# LANGUAGE TemplateHaskell            #-}
-{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Main where
 
+import Control.Monad.Logger (LoggingT, runStdoutLoggingT)
 import Control.Monad.Trans
 import Data.Aeson hiding (json)
 import Data.IORef
 import Data.Monoid ((<>))
 import Data.Text (Text, pack)
 import qualified Data.Text as T
-import GHC.Generics
-import Web.Spock
-import Web.Spock.Config
-import Control.Monad.Logger (LoggingT, runStdoutLoggingT)
-import Database.Persist hiding (get) -- To avoid a naming clash with Web.Spock.get
-import qualified Database.Persist as P -- We'll be using P.get later for GET /people/<id>.
+
+import Data.Text.Encoding
+import Database.Persist hiding (get)
+import qualified Database.Persist as P
 import Database.Persist.Sqlite hiding (get)
 import Database.Persist.TH
+import GHC.Generics
 import Network.HTTP.Types.Status
-import Data.Text.Encoding
+import Web.Spock
+import Web.Spock.Config
 
-share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+share
+  [mkPersist sqlSettings, mkMigrate "migrateAll"]
+  [persistLowerCase|
 Person json -- The json keyword will make Persistent generate sensible ToJSON and FromJSON instances for us.
   name Text
   age Int
@@ -37,6 +40,7 @@ Person json -- The json keyword will make Persistent generate sensible ToJSON an
 |]
 
 type Api = SpockM SqlBackend () () ()
+
 type ApiAction a = SpockAction SqlBackend () () a
 
 main :: IO ()
@@ -87,9 +91,8 @@ app =
           Web.Spock.setStatus notFound404
           errorJson 4 "Could not find a person with matching id"
         Just thePerson -> do
-          runSQL $ P.delete personId  :: ApiAction ()
+          runSQL $ P.delete personId :: ApiAction ()
           json $ object ["result" .= String "success"]
-
 
 runSQL :: (HasSpock m, SpockConn m ~ SqlBackend) => SqlPersistT (LoggingT IO) a -> m a
 runSQL action = runQuery $ \conn -> runStdoutLoggingT $ runSqlConn action conn
@@ -97,10 +100,10 @@ runSQL action = runQuery $ \conn -> runStdoutLoggingT $ runSqlConn action conn
 errorJson' :: Status -> ActionCtxT ctx IO ()
 errorJson' (Status code message) = errorJson code (decodeUtf8 message)
 
-errorJson  :: MonadIO m => Int -> Text -> ActionCtxT ctx m ()
+errorJson :: MonadIO m => Int -> Text -> ActionCtxT ctx m ()
 errorJson code message =
   json $
     object
-    [ "result" .= String "failure"
-    , "error" .= object ["code" .= code, "message" .= message]
-    ]
+      [ "result" .= String "failure",
+        "error" .= object ["code" .= code, "message" .= message]
+      ]
